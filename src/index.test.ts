5d5bcc6a44f777c98469617ebfdce83f7128e2bc
Hideo Yasumoto <hideo54@hideo54.com>
2023-05-10 17:18:01 +0900

    Add test cases

--- src/index.test.ts
+++ src/index.test.ts
@@ -107,6 +107,7 @@ describe('searchCodeInfo', () => {
 
 describe('coordinateToMeshCode', () => {
     it('Returns correct mesh code', () => {
+        // https://www.arcgis.com/apps/instant/lookup/index.html?appid=ec8abf80f76c4417b01561e303ed2d32
         expect(
             mesh.coordinateToMeshCode([
                 35.67548558730212,
@@ -120,6 +121,24 @@ describe('coordinateToMeshCode', () => {
                 35.67548558730212,
             ], '4')
         ).toEqual('533945191');
+
+        // https://www.zenrin.co.jp/product/article/map-mesh/index.html
+        expect(
+            mesh.coordinateToMeshCode([
+                35.67258429819781,
+                139.75400437702388,
+            ], 'JIS-3-1/4')
+        ).toEqual('5339460032');
+
+        // https://netfuku-yaro.com/2021/02/07/post-1046/
+        expect(
+            mesh.coordinateToMeshCode([
+                35.689450,
+                139.691774,
+            ], 'JIS-3-1/8')
+        ).toEqual('53394525323');
+
+        // https://home.csis.u-tokyo.ac.jp/~nishizawa/teikyo/chiiki_mesh.pdf
         expect(
             mesh.coordinateToMeshCode([
                 35.664742,
